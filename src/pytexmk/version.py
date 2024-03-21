622030f5a29286a0a0e02ba2e0ab909671290e4b
焱铭 <lxb-yanming@foxmail.com>
2024-03-21 23:04:52 +0800

    更新版本号

--- src/pytexmk/version.py
+++ src/pytexmk/version.py
@@ -16,7 +16,7 @@
  -----------------------------------------------------------------------
 Author       : 焱铭
 Date         : 2024-03-01 15:52:28 +0800
-LastEditTime : 2024-03-16 21:16:01 +0800
+LastEditTime : 2024-03-21 23:04:42 +0800
 Github       : https://github.com/YanMing-lxb/
 FilePath     : /PyTeXMK/src/pytexmk/version.py
 Description  : 
@@ -26,4 +26,4 @@ Description  :
 #!/usr/bin/env python
 
 script_name = 'PyTeXMK'
-__version__ = '0.2.20.240316'
+__version__ = '0.3.0.240321'
