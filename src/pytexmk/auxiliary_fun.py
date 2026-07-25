aff675b539bbb791b1644b51f276c0ad44f3e6f4
YanMing-CX <lxb-yanming@foxmail.com>
2026-07-25 16:41:43 +0800

    refactor(auxiliary_fun): 重构获取应用路径和退出函数的代码
    调整了get_app_path函数的逻辑顺序，新增对PyInstaller打包环境的支持，统一使用双引号字符串，优化代码排版和可读性

--- src/pytexmk/auxiliary_fun.py
+++ src/pytexmk/auxiliary_fun.py
@@ -1,23 +1,24 @@
 import sys
-from rich import print
 from pathlib import Path
+
+from rich import print
 
 from pytexmk.language import set_language
 
-_ = set_language('auxiliary_fun')
+_ = set_language("auxiliary_fun")
+
+
 # --------------------------------------------------------------------------------
 # 获取 PyTeXMK 路径
 # --------------------------------------------------------------------------------
 def get_app_path():
-    # Nuitka 打包后的路径处理
-    if getattr(sys, 'frozen', False):  # 判断程序是否被打包冻结
-        # Nuitka 打包后资源路径
-        app_path = Path(sys.executable).parent
-    else:
-        # 程序未被打包冻结
-        import importlib.resources  # 用于访问打包资源
-        app_path = Path(importlib.resources.files('pytexmk'))  # 使用 pathlib 获取包数据路径
-    return app_path
+    if hasattr(sys, "_MEIPASS"):
+        return Path(sys._MEIPASS)
+    if getattr(sys, "frozen", False):
+        return Path(sys.executable).parent
+    import importlib.resources
+    return Path(importlib.resources.files("pytexmk"))
+
 
 # --------------------------------------------------------------------------------
 # 定义 PyTeXMK 退出函数
