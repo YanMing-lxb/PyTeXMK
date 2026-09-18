e8ac1409a7393602e95a8ce8f9a3e0da9cfb11f6
YanMing-CX <lxb-yanming@foxmail.com>
2026-09-18 21:11:24 +0800

    feat: 更新退出函数以支持自定义退出码，增强生命周期管理

--- src/pytexmk/lifecycle.py
+++ src/pytexmk/lifecycle.py
@@ -7,8 +7,16 @@ from pytexmk.language import set_language
 
 _ = set_language("lifecycle")
 
+# 退出码约定
+EXIT_OK = 0
+EXIT_ERROR = 1
+EXIT_PROJECT_NOT_FOUND = 3
+EXIT_CONFIG_ERROR = 4
+EXIT_COMPILE_FAILED = 5
 
-def exit_pytexmk():
-    """打印退出提示并以 sys.exit 终止 PyTeXMK 进程。"""
-    print(_("[bold red]正在退出 PyTeXMK..."))
-    sys.exit()
+
+def exit_pytexmk(exit_code: int = EXIT_OK):
+    """打印退出提示并以指定退出码终止 PyTeXMK 进程。"""
+    if exit_code != EXIT_OK:
+        print(_("[bold red]正在退出 PyTeXMK..."))
+    sys.exit(exit_code)
